bc37c6a613592c263e190e9bb1477f41e46d5e49
Mohammed Kareem <kareeeeem@gmail.com>
2017-02-01 17:02:08 +0100

    ex 1.13

--- ch1/exericises.rkt
+++ ch1/exericises.rkt
@@ -1,6 +1,7 @@
 #lang sicp
 
-; Exercise 1.1
+; EXERCISE 1.1
+; ============
 10 ; 10
 (+ 5 3 4) ; 12
 (- 9 1) ; 8
@@ -20,9 +21,9 @@
          ((< a b) b)
          (else -1))
    (+ a 1)) ; 16
-; ---
 
-; Exercise 1.2
+; EXERCISE 1.2
+; ============
 (/
   (+ 5
      4
@@ -34,9 +35,9 @@
      (- 6 2)
      (- 2 7))
   )
-; ---
 
-; Exercise 1.3
+; EXERCISE 1.3
+; ============
 (define (square x)
   (* x x))
 
@@ -50,16 +51,16 @@
   (if (>= a b)
     (sum-of-squares a (if (>= b c) b c))
     (sum-of-squares b (if (>= a c) a c))))
-; ---
 
-; Exercise 1.4
+; EXERCISE 1.4
+; ============
 ; if b is larger than 0 the operation is PLUS otherwise MINUS.
 ; this is equivalent to (+ a (abs b))
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
-; ---
 
-; Exercise 1.5
+; EXERCISE 1.5
+; ============
 (define (p) (p))
 (define (test x y)
   (if (= x 0) 0 y))
@@ -69,15 +70,15 @@
 ; to evaluate (p) which will keep evaluating to (p) indefinitely causing an
 ; infinite loop. Normal-order will only evaluate when the value is needed.
 ; In this case the value is never needed so (test 0 (p)) evaluates to 0.
-; ---
 
-; Exercise 1.6
+; EXERCISE 1.6
+; ============
 ; With new-if the interpreter will always evaluate all it's arguments, and thus
 ; will keep calling sqrt-iter causing an infinite loop.
 ; Newtons method for improving the guess of nth root of x.
-; ---
 
-; Exercise 1.7
+; EXERCISE 1.7
+; ============
 (define (abs x)
   (cond ((> x 0) x)
         ((= x 0) 0)
@@ -131,14 +132,14 @@
 ; good-enough? would result in an infinite loop with very large numbers,
 ; good-enough2? does not. This is because the computer cannot accurately
 ; represent small differences between very large numbers.
-; ---
 
-; Exercise 1.8
+; EXERCISE 1.8
+; ============
 ; See the previous exercise for the definition of nroot. I've tried to make it
 ; general but perhaps this is not the right way.
-; ---
 
-; Exercise 1.9
+; EXERCISE 1.9
+; ============
 ; (define (+ a b)
 ;   (if (= a 0) b (inc (+ (dec a) b))))
 ; (+ 4 5)
@@ -159,9 +160,9 @@
 ; (+ 1 8)
 ; (+ 0 9)
 ; 9
-; ---
 
-; Exercise 1.10
+; EXERCISE 1.10
+; =============
 (define (A x y)
   (cond ((= y 0) 0)
         ((= x 0) (* 2 y))
@@ -176,7 +177,8 @@
 (define (g n) (A 1 n)) ; 2n when n > 0, 0 when n = 0
 (define (h n) (A 2 n)) ; 2^h(n-1) when n > 1, 2 when n == 1, 0 when n == 0
 
-; Exercise 1.11
+; EXERCISE 1.11
+; =============
 ; This is pretty much the exact same thing as fibonachi.
 (define (f-recursive n)
   (if (< n 3)
@@ -192,17 +194,17 @@
       (iter b c (+ a b c) (dec test))))
   (iter 3 3 3 n))
 
-; ---
 
-; Exercise 1.12
+; EXERCISE 1.12
+; =============
 (define (pascal row col)
   (cond ((or (< col 1) (< row col)) #f)
         ((or (= col 1) (= col row)) 1)
         (else (+ (pascal (dec row) col)
                  (pascal (dec row) (dec col))))))
-; ---
 
-; Exercise 1.13
+; EXERCISE 1.13
+; =============
 ; https://en.wikipedia.org/wiki/Mathematical_induction
 (define (fib n)
   (define (iter a b count)
@@ -214,45 +216,59 @@
 (define sqrt5 (sqrt 5))
 (define psi (/ (+ 1 sqrt5) 2))
 (define phi (/ (- 1 sqrt5) 2))
-(define (closest n) (/ (pow psi n) sqrt5))
-(define (prove-for-n n)
-  (= (fib n)
-     (/ (- (pow psi n) (pow phi n))
-        sqrt5)))
 
 ; 1. The basis (base case): prove that the statement holds for the first
 ; natural number n. Usually, n = 0 or n = 1, rarely, n = –1 (although not a
 ; natural number, the extension of the natural numbers to –1 is still a
 ; well-ordered set).
-(prove-for-n 0) ; #t
 
 ; 2. The inductive step: prove that, if the statement holds for some natural
 ; number n, then the statement holds for n + 1.
-((lambda (n) (prove-for-n (inc n))) 5) ; #t
+
+(define (prove f n)
+  (and
+    (f 0)         ; Base case, n = 0
+    (f n)         ; n, for any natutal number
+    (f (+ n 1)))) ; Inductive step, n + 1
+
+(define (test-fib-equals n)
+  (= (fib n)
+     (/ (- (pow psi n)
+           (pow phi n))
+        sqrt5)))
+(prove test-fib-equals 5)
 
 ; For the following I read the following for reference. I am not familiar
 ; with mathematical proofs.
 ; http://www.billthelizard.com/2009/12/sicp-exercise-113-fibonacci-and-golden.html
 ;
-; Now prove that (closest n is infact the closest integer to (fib n)
-; We can rewrite the internals for prove-for-n as follows
+; Now prove that (/ (pow psi n) sqrt5) is infact the closest integer to (fib n)
+; We can rewrite the internals for test-fib-equals follows
+(define (test-fib-equals2 n)
+  (< (- (- (fib n)
+           (/ (pow psi n) sqrt5))
+        (* -1 (/ (pow phi n) sqrt5))) ; The difference
+     0.000001))                       ; because precision is not perfect.
+(prove test-fib-equals2 5)
 
-(lambda (n)
-  (= (- (fib n)
-        (/ (pow psi n) sqrt5))
-     (/ (pow phi n) sqrt5))) ; The difference
+; To prove that (fib n) is the closest integer to (/ (pow psi n) sqrt5) we have
+; to prove that (/ (pow phi n) sqrt5) is always smaller than (/ 1 2).
 
-; To prove that (fib n) is the closet integer to (/ (pow psi n) sqrt5)
-; we have to prove that the difference is always smaller than (/ 1 2)
+(define (test-diff-less-than-half n)
+  (< (/ (pow phi n) sqrt5)
+     (/ 1 2)))
+(prove test-diff-less-than-half 5)
 
-(lambda (n) (< (/ (pow phi n) sqrt5)
-               (/ 1 2)))
-
-(lambda (n) (< (pow phi n)
-              (/ sqrt5 2))) ; 1.118...
+(define (test-diff-less-than-half2 n)
+  (< (pow phi n)
+     (/ sqrt5 2))) ; 1.118...
+(prove test-diff-less-than-half2 5)
 
 ; phi is -0.618304...
-; (pow phi n) will never exceed 1 for postive n.
-; (pow phi n) will never exceed (/ sqrt 5) for postive n.
-; This the above tests always return true.
+
+(define (phi-to-nth-less-or_equal n)
+  (<= (pow phi n) 1))
+(prove phi-to-nth-less-or_equal 5)
+
+; (pow phi n) will never exceed 1 for postive n. And (< 1 (/ sqrt5 2))
 ; Thus (fib n) is the closest integer to (/ (pow psi n) sqrt5)
